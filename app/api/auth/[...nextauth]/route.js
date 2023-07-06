c82377a2c1b188ff84455f3ff526e87df028c129
iambstha <iambstha@gmail.com>
2023-07-07 03:39:58 +0545

    Sign in/out with google implemented

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+    // https://next-auth.js.org/configuration/providers/oauth
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    callbacks: {
+        async jwt({ token }) {
+          token.userRole = "admin"
+          return token
+        },
+      },
+})
+
+// export default NextAuth(authOptions)
+export {handler as GET, handler as POST}
+
