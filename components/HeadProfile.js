cb7275cd3428b9873234fb40782c40f487e21996
iambstha <iambstha@gmail.com>
2023-07-07 05:22:08 +0545

    tweaks for google sign in

--- components/HeadProfile.js
+++ components/HeadProfile.js
@@ -1,5 +1,6 @@
 'use client'
 import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 import React from 'react'
 
 const HeadProfile = () => {
@@ -9,8 +10,16 @@ const HeadProfile = () => {
     }
     else {
         return (
-            <div>
-                {session && <><span>Signed in as {session && session?.user.name}</span></>}
+            <div className=' flex items-center gap-4 mb-2 '>
+                {session?.user && session.user.image && (
+                    <Image
+                        src={session.user.image}
+                        width={40}
+                        height={40}
+                        className=' border rounded-full overflow-hidden '
+                    />
+                )}
+                <div>{session && <><span>Hello {session && session?.user.name}</span></>}</div>
             </div>
         )
     }
