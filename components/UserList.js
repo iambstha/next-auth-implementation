d7eb2fc9128d56ff1f870cfd8441a0ff7c457f28
iambstha <iambstha@gmail.com>
2023-07-07 07:20:06 +0545

    User list UI designed

--- components/UserList.js
+++ components/UserList.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { connectionToDB } from '@/utils/database'
 import User from '@/models/user'
+import Image from 'next/image'
 
 const UserList = async () => {
-    await connectionToDB()
-    const list = await User.find()
-
-    console.log(list[0].email)
-
+  await connectionToDB()
+  const list = await User.find()
   return (
     <div>
       <h2>Total users: {list.length}</h2>
       <ul>
-      {list && list.map(d => <li className=' text-sm text-slate-500 ' key={d.id}>{d.username}</li> )}
+        <table className=' border' >
+          <tr className=' border text-slate-700 '>
+            <th className='p-2 border'>Profile Image</th>
+            <th className='p-2 border'>Username</th>
+            <th className='p-2 border'>Email</th>
+          </tr>
+          {list && list.map(d =>
+            <tr key={d.id} className=' border text-slate-500 '>
+              <td className=' p-2 flex justify-center '><div className=' rounded-full overflow-hidden '><Image src={d.image} width={40} height={40} className='rounded-full' /></div></td>
+              <td className=' p-2 border '>{d.username}</td>
+              <td className=' p-2 border '>{d.email}</td>
+            </tr>
+          )}
+        </table>
       </ul>
     </div>
   )
