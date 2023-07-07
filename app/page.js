503811b60af0f013b19c8fa2e111ca1d0c40af5a
iambstha <iambstha@gmail.com>
2023-07-07 18:53:59 +0545

    original next auth example referenced

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default async function Home() {
 
@@ -5,6 +6,7 @@ export default async function Home() {
   return (
     <div>
       <h2 className=" text-2xl ">Homepage</h2>
+      <p>Highly motivated by the original next auth <Link href='https://next-auth-example.vercel.app/' className=" text-blue-500 hover:underline " target="_blank">example</Link> site </p>
     </div>
   )
 }
