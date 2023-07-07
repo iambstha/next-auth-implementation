884f50e0824e4fcd8ddeec4eb85b3878477fbb4e
iambstha <iambstha@gmail.com>
2023-07-07 19:00:26 +0545

    some changes

--- app/admin/page.js
+++ app/admin/page.js
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <p className=" text-2xl ">This page can be accessed only by admin. This page is using <a target='_blank' href="https://next-auth.js.org/configuration/nextjs#middleware" className=' text-blue-600 hover:underline '>middleware</a>. </p>
+        <p className=" text-2xl ">This page can be accessed only by admin. </p>
+        <p>This page is using <a target='_blank' href="https://next-auth.js.org/configuration/nextjs#middleware" className=' text-blue-600 hover:underline '>middleware</a>.</p>
         <p>Try signing out & visiting this page.</p>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <div>
       <h2 className=" text-2xl ">Homepage</h2>
-      <p>Highly motivated by the original next auth <Link href='https://next-auth-example.vercel.app/' className=" text-blue-500 hover:underline " target="_blank">example</Link> site </p>
+      <p>Highly motivated by the original next auth <Link href='https://next-auth-example.vercel.app/' className=" text-blue-500 hover:underline " target="_blank">example</Link> site. </p>
     </div>
   )
 }
