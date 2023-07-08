060ea62f6b02d06768f298b9628c09f884c257fd
iambstha <iambstha@gmail.com>
2023-07-08 17:46:46 +0545

    mongo issue

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
         <ul className=' flex flex-wrap gap-4 text-blue-900 font-semibold mt-2 md:mt-4 items-center'>
             <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/'>Home</Link></li>
             <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/profile'>Profile</Link></li>
-            <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/user-list'>User Lists</Link></li>
+            {/* <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/user-list'>User Lists</Link></li> */}
             <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/data-without-session'>Without Session</Link></li>
             <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/data-with-session'>With Session</Link></li>
             <li className=' hover:underline '><Link className=' p-0 md:p-2 ' href='/admin'>Admin</Link></li>
